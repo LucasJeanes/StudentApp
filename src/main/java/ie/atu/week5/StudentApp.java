0cd8e4098c587128b7f41ded9a4ce07b54ca9ccb
Lucas Jeanes <lucasjeanes@gmail.com>
2022-10-27 23:22:42 +0100

    Finalized program and cleaned up info print at the end

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -6,57 +6,61 @@ public class StudentApp {
 
     public static void main(String[] args) {
 
+        String name, course, email;
+
         //Student 1 Creation & Data Entry
 
         System.out.println("Please enter the name of the student: ");
         Scanner input = new Scanner(System.in);
-        String name = input.next();
+        name = input.next();
         StudentDetails Student1 = new StudentDetails();
         Student1.setName(name);
-        System.out.println("You entered: " + Student1.getName());
 
         System.out.println("Please enter the email: ");
-        String email = input.next();
+        email = input.next();
         Student1.setEmail(email);
-        System.out.println("You entered: " + Student1.getEmail());
 
         System.out.println("Please enter the course: ");
-        String course = input.next();
+        course = input.next();
         Student1.setCourse(course);
-        System.out.println("You entered: " + Student1.getCourse());
 
         //Student 2 Creation & Data Entry
 
         System.out.println("\nPlease enter the name of the second student: ");
         name = input.next();
         StudentDetails Student2 = new StudentDetails(name);
-        System.out.println("You entered: " + Student1.getName());
 
         System.out.println("Please enter the email: ");
         email = input.next();
         Student2.setEmail(email);
-        System.out.println("You entered: " + Student2.getEmail());
 
         System.out.println("Please enter the course: ");
         course = input.next();
         Student2.setCourse(course);
-        System.out.println("You entered: " + Student2.getCourse());
 
         //Student 3 Creation & Data Entry
 
         System.out.println("\nPlease enter the name of the third student: ");
         name = input.next();
-        System.out.println("You entered: " + name);
 
         System.out.println("Please enter the email: ");
         email = input.next();
-        System.out.println("You entered: " + email);
 
         System.out.println("Please enter the course: ");
         course = input.next();
-        System.out.println("You entered: " + course);
         StudentDetails Student3 = new StudentDetails(name, email, course);
 
+        //Once all student details are entered, print to console
 
+        System.out.println("\nAll student details entered.");
+
+        System.out.println("---Student 1---");
+        System.out.println("Name: " + Student1.getName() + "\nEmail: " + Student1.getEmail() + "\nCourse: " + Student1.getCourse());
+
+        System.out.println("---Student 2---");
+        System.out.println("Name: " + Student2.getName() + "\nEmail: " + Student2.getEmail() + "\nCourse: " + Student2.getCourse());
+
+        System.out.println("---Student 3---");
+        System.out.println("Name: " + Student3.getName() + "\nEmail: " + Student3.getEmail() + "\nCourse: " + Student3.getCourse());
     }
 }
